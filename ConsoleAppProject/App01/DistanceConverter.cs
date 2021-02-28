3740a009a3f7a70007565f9d1d372ab931a86ad7
Feschnne <fescholne@gmail.com>
2021-02-28 19:31:25 +0100

    FeatureStage2Completed.

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -12,43 +12,102 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
-        string valueToConvert;
+        double milesFromFeet;
+        double feetFromMiles;
         double miles;
+        double milesForMts;
+        double miles2;
+        double metresFromMiles;
         double feet;
         public void Run() /**/
         {
             OutputHeading(); /**/
-            InputMiles();
-            Calculate();
+            InputDistances();
             OutputResult();
         }
 
         private void OutputHeading() /**/
         {
-            Console.WriteLine("======================");
-            Console.WriteLine("= DISTANCE CONVERTER =");
-            Console.WriteLine("=    Miles & feet    =");
-            Console.WriteLine("=   by FELIPE SCHOL  =");
-            Console.WriteLine("======================");
+            Console.WriteLine("=========================");
+            Console.WriteLine("=   DISTANCE CONVERTER  =");
+            Console.WriteLine("=     Miles to feet     =");
+            Console.WriteLine("=     Feet to Miles     =");
+            Console.WriteLine("=    Miles to Metres    =");
+            Console.WriteLine("=    by FELIPE SCHOL    =");
+            Console.WriteLine("=========================");
             Console.WriteLine();
 
         }
-        private void InputMiles() /**/
+        private void InputDistances() /**/
         {
             Console.Write("Please enter the desired number of miles > "); /**/
-            string valueToConvert = Console.ReadLine(); /**/
-            miles = Convert.ToDouble(valueToConvert); /**/
+            string valueToConvert1 = Console.ReadLine(); /**/
+            miles = Convert.ToDouble(valueToConvert1); /**/
+            feetFromMiles = miles * 5280;
+
+            Console.Write("Please enter the desired number of feet > ");
+            string valueToConvert2 = Console.ReadLine();
+            feet = Convert.ToDouble(valueToConvert2);
+            milesFromFeet = feet / 5280;
+
+
+            Console.Write("Please enter the desired number of Miles > ");
+            string valueToConvert3 = Console.ReadLine();
+            miles2 = Convert.ToDouble(valueToConvert3);
+            metresFromMiles = miles2 * 1609.34;
+
+
         }
-        private void Calculate() /**/
+        
+        private void OutputResult() /**/
         {
-            feet = miles * 5280;
+            Console.WriteLine();
+            Console.WriteLine(miles + " Miles is " + feetFromMiles + " Feet."); /**/
+            Console.WriteLine(feet + " Feet is " + milesFromFeet + " Miles");
+            Console.WriteLine(miles2 + " Miles is " + metresFromMiles + " Metres");
         }
-        private void OutputResult() /**/ 
-        {
-            Console.WriteLine(); 
-            Console.WriteLine(miles + " Miles is " + feet + " feet."); /**/
-        }
+
+
 
 
     }
 }
+
+//FEATURE STAGE 1-->
+///
+//string valueToConvert;
+//double miles;
+//double feet;
+//public void Run() /**/
+//{
+//    OutputHeading(); /**/
+//    InputMiles();
+//    Calculate();
+//    OutputResult();
+//}
+
+//private void OutputHeading() /**/
+//{
+//    Console.WriteLine("======================");
+//    Console.WriteLine("= DISTANCE CONVERTER =");
+//    Console.WriteLine("=    Miles & feet    =");
+//    Console.WriteLine("=   by FELIPE SCHOL  =");
+//    Console.WriteLine("======================");
+//    Console.WriteLine();
+
+//}
+//private void InputMiles() /**/
+//{
+//    Console.Write("Please enter the desired number of miles > "); /**/
+//    string valueToConvert = Console.ReadLine(); /**/
+//    miles = Convert.ToDouble(valueToConvert); /**/
+//}
+//private void Calculate() /**/
+//{
+//    feet = miles * 5280;
+//}
+//private void OutputResult() /**/ 
+//{
+//    Console.WriteLine(); 
+//    Console.WriteLine(miles + " Miles is " + feet + " feet."); /**/
+//}
